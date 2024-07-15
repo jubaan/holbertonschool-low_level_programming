41e3f6095895896c0db5cfabb8fb1d5a8b4ad3a7
Julio Añoveros <julio.ab@outlook.com>
2024-07-15 14:26:29 -0600

    Add: 2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the alphabet in lowercase, followed by a
+ * new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	return (0);
+}
+
