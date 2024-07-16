d2086a8b8bf00daa6a7ac22dbb6970a07b5d75a5
Julio Añoveros <julio.ab@outlook.com>
2024-07-15 22:33:34 -0600

    Add: 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
-#include <stdio.h>
+#include "main.c"
 
 /**
  * main - Entry point
  *
- * Description: This program prints "__putchar"
+ * Description: This program prints "_putchar"
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("__putchar\n");
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (i < sizeof(str[]) - 1)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar("\n");
 	return (0);
 }
 
