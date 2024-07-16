7e8ec3a8f702e53efd6ee269a6cdcf501adc194f
Julio Añoveros <julio.ab@outlook.com>
2024-07-15 22:39:58 -0600

    a

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,17 +8,17 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int _putchar(void)
 {
 	char str[] = "_putchar";
 	int i = 0;
 
 	while (i < sizeof(str[]) - 1)
 	{
-		_putchar(str[i]);
+		write(1, str[i], 1);
 		i++;
 	}
-	_putchar("\n");
+	write(1, "\n", 1);
 	return (0);
 }
 
