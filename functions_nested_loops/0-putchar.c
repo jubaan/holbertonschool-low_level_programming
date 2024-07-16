e4e4076333133585da7c0164d83dc81937d6f00a
Julio Añoveros <julio.ab@outlook.com>
2024-07-15 18:44:56 -0600

    Add: 0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints "__putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("__putchar\n");
+	return (0);
+}
+
