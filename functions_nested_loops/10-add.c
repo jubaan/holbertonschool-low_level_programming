47ab1bdeb028162578fd6c043d9a3489d047391c
Julio Añoveros <julio.ab@outlook.com>
2024-07-17 10:25:54 -0600

    Add: 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - This functions adds two integers and returns the result
+ * @a: Firsts summand
+ * @b: Second summand
+ *
+ * Return: Addition
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
