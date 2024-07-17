ed1963beb197c64265c73530f744e448d1a30f5a
Julio Añoveros <julio.ab@outlook.com>
2024-07-16 20:23:12 -0600

    Add: 3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - This function checks for lowercase characters
+ * @c: Stores the character to be reviewed
+ *
+ * Return: 1 for TRUE or 0 for FALSE
+ */
+
+int _islower(int c)
+{
+	&c = (int)c
+	if c >= 97 && c <= 122
+		return (1);
+	else
+		return (0);
+}
+
