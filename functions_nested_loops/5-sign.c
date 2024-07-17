4f52582ad90ecb80617491372a67643e78c1ac1a
Julio Añoveros <julio.ab@outlook.com>
2024-07-16 22:19:27 -0600

    Add: 5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - This function prints the sign of a number
+ * @n: is the number the function uses to define the sign
+ *
+ * Return: 1, -1 or 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
