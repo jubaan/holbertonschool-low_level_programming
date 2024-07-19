fd64366b39b0d47a7792b3e07e2cb3983cc88203
Julio Añoveros <julio.ab@outlook.com>
2024-07-18 22:48:57 -0600

    Add: 5-more_numbers.c files

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - This function prints 10 times the numbers from 0 to 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = 0;
+
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
