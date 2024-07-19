16b108ad33e541a8d9bf0a6e9cb1d51add6bf699
Julio Añoveros <julio.ab@outlook.com>
2024-07-18 21:08:47 -0600

    Add: 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * mul - This function multiplies two integers
+ * @a: multiplicand
+ * @b: multiplier
+ *
+ * Return: The product of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
