c489392caf8299f25029bcded028c3c176f26976
Julio Añoveros <julio.ab@outlook.com>
2024-07-18 19:44:26 -0600

    a

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,17 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-		n = -n;
+	if (n == INT_MIN)
+	{
+		last_digit = 8;
+	}
+	else
+	{
+		if (n < 0)
+			n = -n;
 
-	last_digit = n % 10;
+		last_digit = n % 10;
+	}
 
 	_putchar(last_digit + '0');
 	return (last_digit);
