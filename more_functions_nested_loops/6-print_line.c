b97622e1e9c7e07169011b5fd47df64395fdab0a
Julio Añoveros <julio.ab@outlook.com>
2024-07-18 23:05:08 -0600

    a

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - This function draws a straight line in the terminal
+ * @n: Number of repeated underscores
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+
+	while (n > 0)
+	{
+		_putchar('_');
+		i--;
+	}
+}
+
