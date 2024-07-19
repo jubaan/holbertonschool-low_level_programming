a680b47e5d1f8786c39bf06c69a057092f5dec0d
Julio Añoveros <julio.ab@outlook.com>
2024-07-18 21:14:48 -0600

    Add: 3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - This function prints the numbers from 0 to 9
+ *
+ * Return: Void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}	
+
+	printf("\n");
+	return (void);
+}
+
