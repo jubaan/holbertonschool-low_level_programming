1820259338c82627485232031036ce4b4964ae36
Julio Añoveros <julio.ab@outlook.com>
2024-07-18 22:40:06 -0600

    a

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - This function prints the numbers
+ * from 0 to 9, except 2 and 4
+ *
+ * Return: Void
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 || i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
