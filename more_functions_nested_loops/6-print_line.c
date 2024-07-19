f7ba4d0c8d06c245acd6d8525e9d6eee800dab1f
Julio Añoveros <julio.ab@outlook.com>
2024-07-18 23:16:05 -0600

    a

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,13 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-		_putchar('\n');
-
-	while (n > 0)
+	if (n > 0)
 	{
-		_putchar('_');
-		n--;
-	}
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
 
 	_putchar('\n');
 }
