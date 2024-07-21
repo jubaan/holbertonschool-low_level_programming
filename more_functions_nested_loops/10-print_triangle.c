51e53131a17844bda30b8a8ed66745abcce8a5f1
Julio Añoveros <julio.ab@outlook.com>
2024-07-21 11:20:06 -0600

    Add: 10-print_triangle.c file

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,41 @@
+#include "main.h";
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+    int i = 0;
+    int j;
+    int k;
+
+    if (size <= 0)
+        _putchar('\n');
+    else
+    {
+        while (i < size)
+        {
+            j = size - 1;
+            k = 0;
+
+            while (j > i)
+            {
+                _putchar(' ');
+                j--;
+            }
+
+            while (k <= i)
+            {
+                _putchar('#');
+                k++;
+            }
+
+            _putchar('\n');
+            i++;
+        }
+    }
+}
