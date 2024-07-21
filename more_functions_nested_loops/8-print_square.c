4c032db6b7facd11f2bc69e9bf5abefc597e76d8
Julio Añoveros <julio.ab@outlook.com>
2024-07-21 11:01:25 -0600

    Add: 8-print_square.c file

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+    int i = 0;
+    int j;
+
+    if (size <= 0)
+        _putchar('\n');
+    else
+    {
+        while (i < size)
+        {
+            j = 0;
+
+            while (j < size)
+            {
+                _putchar('#');
+                j++;
+            }
+            _putchar('\n');
+            
+            i++;
+        }
+    }
+}
