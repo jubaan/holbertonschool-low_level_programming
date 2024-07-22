1eb3cfc58ab82d002ac004c3638bc5b565642545
Julio Añoveros <julio.ab@outlook.com>
2024-07-22 09:40:10 -0600

    a

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -20,13 +20,8 @@ void fizz_buzz(void)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (i % 5 == 0)
+		else (i % 5 == 0)
 			printf("Buzz");
-		else
-			printf("%d", i);
-
-		if (i != 100)
-			printf(" ");
 
 		i++;
 	}
