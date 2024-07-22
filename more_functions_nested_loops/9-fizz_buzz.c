7b68c739ee93c26e5a111daa7dc58d608229794b
Julio Añoveros <julio.ab@outlook.com>
2024-07-22 11:42:54 -0600

    a

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,7 +6,6 @@
  * For multiples of three print Fizz instead of the number
  * For the multiples of five print Buzz
  * For numbers which are multiples of both three and five print FizzBuzz
- * Return: 0
  */
 
 void fizz_buzz(void)
@@ -28,5 +27,5 @@ void fizz_buzz(void)
 	}
 
 	printf("\n");
-	return (0);
+	return;
 }
