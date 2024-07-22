9e6d2a39c8c8085649679b09cf0ef537ad944e17
Julio Añoveros <julio.ab@outlook.com>
2024-07-22 15:51:39 -0600

    a

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * _strlen: Returns the length of a string
+ * _strlen - Returns the length of a string
  * @s: string
- * 
+ *
  * Return: string length
  */
 
 int _strlen(char *s)
 {
-	
-}
+	int len = 0;
 
+	while (sizeof(s) > 0)
+		len++;
+
+	return (len);
+}
