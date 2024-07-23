4cffbc5e54d1771f46ce510ee3b6c303effd0b39
Julio Añoveros <julio.ab@outlook.com>
2024-07-22 20:22:32 -0600

    Add: 5-rev_string.c file

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = _strlen(*s) - 1;
+	int j = 0;
+	char temp;
+
+	while (i > j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i--;
+		j++;
+	}
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: string
+ *
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
