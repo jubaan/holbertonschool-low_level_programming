0737c6a69abdf3866963fe0f830f899a32df4435
Julio Añoveros <julio.ab@outlook.com>
2024-07-23 13:58:20 -0600

    a

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,18 +2,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * puts2 - prints every other character of a string
  * @str: string to print
  *
