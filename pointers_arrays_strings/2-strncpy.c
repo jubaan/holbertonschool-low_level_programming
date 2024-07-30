ce23564ee0f11f1ae05de754ed168e59ced86290
Julio Añoveros <julio.ab@outlook.com>
2024-07-30 00:59:48 -0600

    Add: 2-strncpy.c file

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes from src to copy
+ *
+ * Return: returns copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && j < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	if (dest[i] == '\0')
+		dest[i] = '\0';
+
+	return (dest);
+}
