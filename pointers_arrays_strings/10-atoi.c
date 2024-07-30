31ca952d4a9a8fba58d81583de29afc5d4482596
Julio Añoveros <julio.ab@outlook.com>
2024-07-29 22:57:06 -0600

    a

--- pointers_arrays_strings/10-atoi.c
+++ pointers_arrays_strings/10-atoi.c
@@ -13,29 +13,26 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int num = 0;
-	int str = 0;
+	int started = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == '-')
+		if (s[i] == '-' && !started)
 		{
 			sign *= -1;
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			str = 1;
-			
-			if (num > INT_MAX / 10 || (num == INT_MAX / 10 && (s[i] - '0') > 7))
+			started = 1;
+
+			if (num > (INT_MAX - (s[i] - '0')) / 10)
 			{
-				if (sign == 1)
-					return (INT_MAX);
-				else
-					return (INT_MIN);
+				return (sign == 1) ? INT_MAX : INT_MIN;
 			}
 
 			num = num * 10 + (s[i] - '0');
 		}
-		else if (num > 0)
+		else if (started)
 		{
 			break;
 		}
