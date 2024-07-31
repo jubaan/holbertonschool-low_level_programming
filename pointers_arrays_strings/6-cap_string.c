ce16fdd04493ce3c626de271aa0b757da2273cc0
Julio Añoveros <julio.ab@outlook.com>
2024-07-31 17:23:16 -0600

    Add: 6-cap_string.c function

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,10 +13,13 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
-		else
-			str[i] = str[i];
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] -= 32;
 
 		i++;
 	}
