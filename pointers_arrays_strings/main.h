3aef96820b3614947dcd8720d15a7ce5b0004b73
Julio Añoveros <julio.ab@outlook.com>
2024-07-31 16:45:50 -0600

    Add: 5-string-toupper.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
+char *string_toupper(char *str);
 
 #endif
