992dac9d5eafd4958e7ca4145d742c3e03f6d73f
Julio Añoveros <julio.ab@outlook.com>
2024-07-31 16:58:06 -0600

    a

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,5 +16,6 @@ char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
+char *cap_string(char *);
 
 #endif
