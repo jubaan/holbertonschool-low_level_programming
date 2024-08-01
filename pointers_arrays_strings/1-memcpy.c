f211d9fef76814d2b56bb8998591b1c61c3b7a5a
Julio Añoveros <julio.ab@outlook.com>
2024-08-01 17:54:46 -0600

    Add: 1-memcpy.c function

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area destination
+ * @src: source memory area
+ * @n: bytes of memory to copy
+ *
+ * Return: a pointer to "dest"
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i]
+		i++;
+	}
+
+	return (dest);
+}
+
