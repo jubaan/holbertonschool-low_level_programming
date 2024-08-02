162e619eb45e5a345a1c1386d667bad84e66009b
Julio Añoveros <julio.ab@outlook.com>
2024-08-01 18:08:29 -0600

    Add: 2-strch.c function

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to find
+ *
+ * Return: a pointer to the first occurence of the character "c" or "NULL"
+ * if is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (s)
+	{
+		if (s == c) ? return (c) : return (NULL);
+
+		s++;
+	}
+}
+
