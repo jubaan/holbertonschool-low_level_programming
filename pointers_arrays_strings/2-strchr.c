17324b251f018e3421557f4d2f2519b615f76cef
Julio Añoveros <julio.ab@outlook.com>
2024-08-01 18:15:48 -0600

    Refactor: return value of char

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -14,7 +15,7 @@ char *_strchr(char *s, char c)
 	while (*s)
 	{
 		if (*s == c)
-			return (c);
+			return (s);
 
 		s++;
 	}
