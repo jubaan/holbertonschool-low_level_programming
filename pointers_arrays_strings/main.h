af3aa215ff8ac1d1dabe68b24b8d67fbef929947
Julio Añoveros <julio.ab@outlook.com>
2024-08-01 18:09:20 -0600

    Update: main.h with function declaration

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,5 +20,6 @@ char *cap_string(char *str);
 char *leet(char *str);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
