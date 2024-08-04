f6f8c06c459c9b8678323a5006c84962f17a550d
Julio Añoveros <julio.ab@outlook.com>
2024-08-03 22:30:42 -0600

    Add: 0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - pritns a string followed by a new line
+ * @s: string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	write(1, s, 1);
+	_puts_recursion(s++);
+}
