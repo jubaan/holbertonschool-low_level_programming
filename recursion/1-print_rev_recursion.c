687d7cae65838d21679ba0cce4d5df10c36d909c
Julio Añoveros <julio.ab@outlook.com>
2024-08-03 22:47:31 -0600

    Add: 1-print_rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		write(1, "\n", 1);
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	write(1, s, 1);
+}
