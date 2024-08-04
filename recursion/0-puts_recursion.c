3248613bef42b6de9d516f1d5a9615ae71e90f67
Julio Añoveros <julio.ab@outlook.com>
2024-08-03 22:38:25 -0600

    a

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -11,10 +11,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		{
-			write(1, "\n", 1);
-			return;
-		}
+	{
+		write(1, "\n", 1);
+		return;
+	}
 
 	write(1, s, 1);
 	_puts_recursion(s + 1);
