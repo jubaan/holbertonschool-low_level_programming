be782fb4508eaa709b77b2bcc2c058af424d982e
Julio Añoveros <julio.ab@outlook.com>
2024-08-14 19:19:32 -0600

    Add: 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#inlcude "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int multiplicand = atoi(argv[1]);
+	int multiplier = atoi(argv[2]);
+	int product = multiplicand * multiplier;
+
+	if (multiplicand == NULL || multiplier == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", product);
+
+	return (0);
+}
