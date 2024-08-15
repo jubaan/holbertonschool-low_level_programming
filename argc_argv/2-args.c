9ee9e0166ac65a234b0512fa81aa09ad8bd07cca
Julio Añoveros <julio.ab@outlook.com>
2024-08-14 19:12:34 -0600

    Add: 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	(void)argc;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
