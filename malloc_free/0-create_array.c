776342bc955a4a03b98ad3a3b33afff9b656a852
Julio Añoveros <julio.ab@outlook.com>
2024-08-19 16:37:06 -0600

    a

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * create_array - creates an array of chars, and initializes it with a specific char
@@ -11,7 +12,7 @@
  char *create_array(unsigned int size, char c)
  {
         char *array;
-        int i = 0;
+        unsigned int i = 0;
 
         if (size == 0)
             return (NULL);
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -4,4 +4,4 @@
 char _putchar(char c);
 char *create_array(unsigned int size, char c);
 
-#endif // MAIN_H
+#endif
